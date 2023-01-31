406b00cb24eeb26e2afa109b9eb873b6d358950a
Ulrich Kaminski <ulrich@klang-games.com>
2023-01-31 11:07:06 +0000

    fix: telepresence url

--- src/telepresence-install/install.sh
+++ src/telepresence-install/install.sh
@@ -12,7 +12,7 @@ apt update && apt install -y --no-install-recommends \
 # install telepresence
 echo "Installing telepresence..."
 
-if [ $(uname -m) = 'x86_64' ]; then echo -n "amdx64" >/tmp/arch; else echo -n "arm64" >/tmp/arch; fi
+if [ $(uname -m) = 'x86_64' ]; then echo -n "amd64" >/tmp/arch; else echo -n "arm64" >/tmp/arch; fi
 ARCH=$(cat /tmp/arch)
 
 URL="https://app.getambassador.io/download/tel2/linux/${ARCH}/${VERSION}/telepresence"
