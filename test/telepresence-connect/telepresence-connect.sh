f1beac63a0da18c712617e61c7d308cdc80c7524
Ulrich Kaminski <ulrich@klang-games.com>
2023-01-31 17:24:49 +0100

    fix: telepresence install and connect

--- test/telepresence-connect/telepresence-connect.sh
+++ test/telepresence-connect/telepresence-connect.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "connect-telepresence --domain seed-dev --project seed-209211 --namespace sharedsvc"
+check "execute command" bash -c "connect-telepresence --domain seed-dev --project seed-209211"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
